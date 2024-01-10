25ec4eebf6bbc3d17de79f53c3c2a36b4466eadd
Piyush Jain <piyushjain@Piyushs-MacBook-Pro.local>
2024-01-10 00:22:12 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import Header from "./components/Header";
+import Results from "./components/Results";
+import UserInput from "./components/UserInput";
+import { useState } from "react";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 15000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChangeInput={handleChange} />
+      {!inputIsValid && <p className="center">Enter Non-Negative Value</p>}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
+}
+
+export default App;
